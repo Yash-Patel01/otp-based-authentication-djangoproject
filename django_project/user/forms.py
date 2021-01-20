7d668e6e8a1c6dd5254c7655017ff1bd83c9e431
Rahul Patel <patelrahul9513@gmail.com>
2021-01-20 15:57:17 +0530

    extended user model and create register form

---
+++ django_project/user/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    phone_number = forms.CharField(max_length=10)
+
+    class Meta:
+        model = User
+        fields = ['phone_number', 'username', 'first_name', 'last_name', 'email', 
+                  'password1', 'password2']
--- django_project/user/views.py
+++ django_project/user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import User
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'user/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form})
